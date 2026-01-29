aec7ad635ac1ff1044a1c52de6211c9e3b911d46
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-01-29 14:11:27 +0100

    fix svelte

--- src/svelte/index.js
+++ src/svelte/index.js
@@ -1,12 +1,17 @@
 import { createSPAFileBasedRouter } from "ziko/router";
+import { mount } from 'svelte'
 
 export const createFileBasedRouter = ({pages, target}) => createSPAFileBasedRouter({
     pages,
     target,
-    extensions: ['js', 'ts'],
-    renderer : async (target, component, props ) => {
-      target.append(await component(props))
-    }
+    extensions : ['svelte'],
+    renderer : (target, component, props)=>{
+      mount(component, {
+        target,
+        props
+      })
+    console.log({target, component, props})
+  }
 })
 
 
