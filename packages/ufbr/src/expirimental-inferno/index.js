3bd15814a07c91f72d046cc3f944a0fc27597bed
zakarialaoui10 <zakarialaoui10@gmail.com>
2026-08-29 15:33:26 +0100

    add experimental inferno

---
+++ packages/ufbr/src/expirimental-inferno/index.js
@@ -0,0 +1,17 @@
+import { createSPAFileBasedRouter } from "ziko/router";
+import { render, createElement } from "inferno";
+
+export const renderer = (target, component, props) => {
+  render(
+    createElement(component, props),
+    target
+  );
+}
+
+export const createFileBasedRouter = ({ pages, target }) =>
+  createSPAFileBasedRouter({
+    pages,
+    target,
+    extensions: ["jsx", "tsx", "js", "ts"],
+    renderer,
+  });
